6f5a682f96dfb7c071ddecf6ae7b1edb6993b31c
Amulya Boggavarapu <82016332+amulyaboggavarapu7@users.noreply.github.com>
2023-10-21 23:32:32 -0500

    users commited

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
+var userRouter=require('./routes/users');
 var mydataRouter = require('./routes/mydata');
 var app = express();
 // view engine setup
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/mydata', mydataRouter);
+app.use('/users',userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
