0c809addd65940c09da22a1a67542a350b0e7ab9
Amulya Boggavarapu <82016332+amulyaboggavarapu7@users.noreply.github.com>
2023-10-23 17:52:25 -0500

    computation endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var userRouter=require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var computation
+var computationRouter=require('./routes/computation')
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/mydata', mydataRouter);
 app.use('/users',userRouter);
+app.use('/computation',computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/computation.js
+++ routes/computation.js
@@ -1,49 +1,9 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var router = express.Router();
 
-
-
-
-app.get('/computation', (req, res) => {
-    const lastDigitOfId = 5; 
-
-    
-    const randomValue = Math.random() * 100; 
-
-    
-    const x = req.query.x || randomValue;
-
-    let fn;
-    switch (lastDigitOfId % 4) {
-        case 0:
-            fn = "square root";
-            x = Math.sqrt(x);
-            break;
-        case 1:
-            fn = "sine";
-            x = Math.sin(x);
-            break;
-        case 2:
-            fn = "cosine";
-            x = Math.cos(x);
-            break;
-        case 3:
-            fn = "tangent";
-            x = Math.tan(x);
-            break;
-    }
-
-    
-    const response = `${fn} applied to ${x} is ${x}`;
-
-    
-    res.send(response);
+/* welcome to computation.pug */
+router.get('/', function(req, res, next) {
+  res.render('computation', { title: 'Amulya Boggavarapu' });
 });
 
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+module.exports = router;
