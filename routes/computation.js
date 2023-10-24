2efe013097cd5253ad3386e11724eacda050ddaf
Amulya Boggavarapu <82016332+amulyaboggavarapu7@users.noreply.github.com>
2023-10-23 22:05:29 -0500

    computation commit

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* welcome to computation.pug */
-router.get('/', function(req, res, next) {
-  res.render('computation', { title: 'Amulya Boggavarapu' });
-});
 
+
+router.get('/', function(req, res, next) {
+  var rand =Math.random().toFixed(2);
+  var y = req.query.x;
+
+  if(y == undefined){
+    y =rand;
+  }
+    a=Math.cos(rand).toFixed(2);
+    b=Math.asin(rand).toFixed(2);
+    c=Math.asinh(rand).toFixed(2);
+    res.send(`Math.cos(${y}): ${a} Math.asin(${y}) is: ${b} Math.asinh(${y}) is : ${c}`);
+    
+    
+    
+    
+    
+
+    
+    
+    
+  });
+  
 module.exports = router;
